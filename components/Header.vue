2245ba801d6aca9368209ccac72e55cfef56df32
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-11-12 19:54:42 +0100

    feat(front-end): add header

--- components/Header.vue
+++ components/Header.vue
@@ -1,19 +1,97 @@
+<script>
+export default {
+	data() {
+		return {
+			openMobileMenu: 0,
+			planet: {
+				mercury: {
+					name: "MERCURY",
+					color: "DEF4FC",
+				},
+				venus: {
+					name: "VENUS",
+					color: "F7CC7F",
+				},
+				earth: {
+					name: "EARTH",
+					color: "545BFE",
+				},
+				mars: {
+					name: "MARS",
+					color: "FF6A45",
+				},
+				jupiter: {
+					name: "JUPITER",
+					color: "ECAD7A",
+				},
+				saturn: {
+					name: "SATURN",
+					color: "FCCB6B",
+				},
+				uranus: {
+					name: "URANUS",
+					color: "65F0D5",
+				},
+				neptune: {
+					name: "NEPTUNE",
+					color: "497EFA"
+				}
+			},
+		};
+	},
+};
+</script>
+
 <template>
-    <header class="header">
-        <div class="logo">
-            <h1>THE PLANETS</h1>
-        </div>
-        <nav>
-            <ul>
-                <li><NuxtLink to="/mercury">MERCURY</NuxtLink></li>
-                <li><NuxtLink to="/venus">VENUS</NuxtLink></li>
-                <li><NuxtLink to="/earth">EARTH</NuxtLink></li>
-                <li><NuxtLink to="/mars">MARS</NuxtLink></li>
-                <li><NuxtLink to="/jupiter">JUPITER</NuxtLink></li>
-                <li><NuxtLink to="/saturn">SATURN</NuxtLink></li>
-                <li><NuxtLink to="/uranus">URANUS</NuxtLink></li>
-                <li><NuxtLink to="/neptune">NEPTUNE</NuxtLink></li>
-            </ul>
-        </nav>
-    </header>
+	<header class="header">
+		<div class="logo">
+			<h1>THE PLANETS</h1>
+		</div>
+		<nav>
+			<ul class="desktop-nav">
+				<li v-for="(value, index) in planet" :key="index">
+					<NuxtLink :to="'/' + index">{{
+						value.name
+					}}</NuxtLink>
+				</li>
+			</ul>
+			<div
+				class="mobile-nav"
+				title="Navigation"
+				v-on:click="openMobileMenu = !openMobileMenu"
+			>
+				<img
+					:src="require('@/assets/images/icon-hamburger.svg')"
+					alt="Navigation"
+				/>
+			</div>
+			<div class="mobile-tab" v-if="openMobileMenu">
+				<ul>
+					<li v-for="(value, index) in planet" :key="index">
+						<div class="left">
+							<div
+								class="planet"
+								:style="'background: #' + value.color"
+							></div>
+							<div class="name">
+								<NuxtLink :to="'/' + index">{{
+									value.name
+								}}</NuxtLink>
+							</div>
+						</div>
+						<div
+							class="right"
+							:title="'Redirection vers ' + value.name"
+						>
+							<img
+								:src="
+									require('@/assets/images/icon-chevron.svg')
+								"
+							/>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</header>
 </template>
